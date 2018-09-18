fd33bd80eee4cb499319c223d8be4dbc86549a7a
alebreux <alexandre.lebreux@gmail.com>
2018-09-18 21:20:11 +0200

    Style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {AngularFireModule} from 'angularfire2';
-import { configFirebase} from '../environments/prod.firebase.config';
-import {AngularFirestoreModule} from 'angularfire2/firestore';
+import { AngularFireModule } from 'angularfire2';
+import { configFirebase } from '../environments/prod.firebase.config';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HistoryComponent } from './history/history.component';
